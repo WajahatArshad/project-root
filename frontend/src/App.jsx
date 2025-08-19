2cebb9cbcd373d42dbd3c767c4b5b7133b438067
Wajahat Arshad <wajahatarshad111@gmail.com>
2025-08-19 18:16:22 +0500

    api modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 console.log(API_URL)
 function App() {
   const [items, setItems] = useState([]);
